b9b087a7b70178981d5f5e422ce45af648b09aca
SonOfMaaJo <naoussiviery237@gmail.com>
2024-09-09 21:30:20 +0200

    The last commit : The entire set of programs has been consolidated into a single file.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import copy
-from powerset import powerset
 import csv
 
+def powerset(N):
+    """
+    Returns the powerset of a set N.
+    """
+    if len(N) == 0:
+        return [set()]
+    else:
+        P = powerset(N[1:])
+        return P + [subset | {N[0]} for subset in P]
+
 def k_partition_generattion(M, N, c):
-    Q = ["s"]
-    Q = Q + list(N - M)
+    """
+        display the partition generate by M.
+        Attention : it is possible for the same partition to 
+        appear when displayed.
+    """
+    Q =  ["s"] + list(N - M)
     P = {y: {y} for y in M}
-    k_partitions = []
 
     def element_compatible(vertex, M, c):
         return list(filter(lambda y: y in c[vertex] and vertex in c[y], M))
-    # fonction de recherche basée sur le backtraccking
+    
+    #search
     def backTrackingSearch(vertex):
         if vertex == "s":
            Q.append(vertex)
-           k_partitions.append(copy.deepcopy(P))
+           print([bloc for bloc in P.values()])
         else:
            for y in element_compatible(vertex, M, c):
                if all((x in c[vertex] and vertex in c[x]) for x in P[y]):
@@ -25,12 +38,18 @@ def k_partition_generattion(M, N, c):
               
                 
     backTrackingSearch(Q.pop())
-    return k_partitions
 
 def main():
+    """
+     generate the set of all compatibility partition of the example 1 and 2 
+     of paper.
+     
+     A partition is a list of subsets of 
+       N containing elements that are pairwise compatible.
+    """
     N = {1, 2, 3, 4, 5}
-    # resolution de la situation 1
-    clique1_partitions = []
+    # Solution of example 1
+    clique1_partitions = set()
     c1 = {}
     c1[1] = {1, 2, 3, 4}
     c1[2] = {1, 2, 4, 5}
@@ -38,16 +57,13 @@ def main():
     c1[4] = {1, 2, 4, 5}
     c1[5] = {1, 3, 5}
     M1_alpha = {2, 3}
+    print("Solution of the example 1 :\n")
     for S in powerset(list(N - M1_alpha)):
-        clique1_partitions.append(k_partition_generattion(M1_alpha | S, N, c1))
-    with(open("clique1_partitions.csv", "w", newline="")) as f:
-        writer = csv.writer(f)
-        for k_partitions in clique1_partitions:
-            writer.writerow([x for x in k_partitions[0].keys()])
-            writer.writerows([k_partition.values() for k_partition in k_partitions])
+        k_partition_generattion(M1_alpha | S, N, c1)
+    
 
-    # resolution de la situation 2
-    clique2_partitions = []
+    # Solution of the example 2
+    clique2_partitions = set()
     c2 = {}
     c2[1] = {1, 3, 4, 6}
     c2[2] = {2, 3, 5}
@@ -56,13 +72,10 @@ def main():
     c2[5] = {2, 3, 4, 5, 6}
     c2[6] = {1, 5, 6}
     M2_alpha = {3, 4, 6}
+    print("\n\nSolution of the example 2 :\n")
     for S in powerset(list(N - M2_alpha)):
-        clique2_partitions.append(k_partition_generattion(M2_alpha | S, N, c2))
-    with(open("clique2_partitions.csv", "w", newline="")) as f:
-        writer = csv.writer(f)
-        for k_partitions in clique2_partitions:
-            writer.writerow([x for x in k_partitions[0].keys()])
-            writer.writerows([k_partition.values() for k_partition in k_partitions])
+        k_partition_generattion(M2_alpha | S, N, c2)
+    
   
 if __name__ == "__main__":
    main()
